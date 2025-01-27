859d09d29b8fb5a22d537419a5cc0b707975e974
shigneedmeds <167283620+shigneedmeds@users.noreply.github.com>
2025-01-28 10:38:08 +1100

    Update PH_Walker.m

--- Operations/PH_Walker.m
+++ Operations/PH_Walker.m
@@ -78,7 +78,7 @@ function out = PH_Walker(y,walkerRule,walkerParams)
 % ------------------------------------------------------------------------------
 %% Preliminaries
 % ------------------------------------------------------------------------------
-doPlot = 0; % plot outputs to figure
+doPlot = 0;%ot outputs to figure
 N = length(y); % the length of the input time series, y
 
 % ------------------------------------------------------------------------------
