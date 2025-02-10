6f423c16600c6b9a54e7ff83d34c6487edf5d742
shigneedmeds <167283620+shigneedmeds@users.noreply.github.com>
2025-02-10 17:40:47 +1100

    Made some smoothed datasets and used a classifier to determine mass vs accuracy for the datasets

---
+++ HCTSA2018_Data/smoother.m
@@ -0,0 +1,39 @@
+set = "Beef";
+dataset = "Normalized/HCTSA_" + set + "_N.mat";
+dataset = convertStringsToChars(dataset);
+
+[TS_DataMat,TimeSeries,Operations,~] = TS_LoadData(dataset);
+
+ts = cell2mat(transpose(TimeSeries.Data));
+labels = double(TimeSeries.Group);
+
+
+time = zeros(size(ts));
+col = size(ts);
+N = col(1);
+col = col(2);
+m = 1;
+
+for j = 1:col
+
+    w = time(:, j);
+    y = ts(:, j);
+
+    w(1) = y(1);
+    w(2) = y(2);
+    for k = 3:N
+        w_inert = w(k-1) + (w(k-1)-w(k-2));
+        w(k) = w_inert + (y(k)-w_inert)/m;
+    end
+
+    time(:, j) = w;
+
+end
+
+time = transpose(time);
+
+result = [labels, time];
+
+writematrix(result, sprintf("Smoothed/%s_smoothed.csv", set));
+
+
